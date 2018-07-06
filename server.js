472633ef4296bed37c8bbe22a717dbd3f291a9fa
Mathew Kurian <bluejamesbond@users.noreply.github.com>
2018-07-05 18:21:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({followRedirects: true});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
